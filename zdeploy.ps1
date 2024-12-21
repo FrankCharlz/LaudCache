60b7ee970d4ff69bc2482d2755f36a7577a5cb11
Frank Magoti <frank.magoti@nhif.or.tz>
2024-12-21 13:58:49 +0300

    !

---
+++ zdeploy.ps1
@@ -0,0 +1,40 @@
+# publish.ps1
+
+# Function to read XML properties
+function Get-XmlProperty {
+    param (
+        [string]$filePath,
+        [string]$propertyName
+    )
+
+    if (Test-Path $filePath) {
+        $xml = [xml](Get-Content $filePath)
+        $prop = $xml.SelectSingleNode("//PropertyGroup/$propertyName")
+        if ($prop) {
+            return $prop.InnerText
+        }
+    }
+    return $null
+}
+
+$buildProps = "Directory.Build.props"
+
+# Read version and packageId
+$version = Get-XmlProperty $buildProps "Version"
+$packageId = Get-XmlProperty $buildProps "PackageId"
+
+
+if (!$version -or !$packageId) {
+    Write-Error "Could not find Version or PackageId in project files"
+    exit 1
+}
+
+Write-Host "Building package $packageId version $version"
+
+# Build and pack
+dotnet build --configuration Release
+dotnet pack --configuration Release
+
+# Push to NuGet feed
+$package = "$packageId.$version.nupkg"
+dotnet nuget push "bin/Release/$package" -s http://mecmis:30011/v3/index.json
